283164de2f6d23bfd54a9dd7f479f691bd020616
vinothsmart <s.vinothkanna90@gmail.com>
2021-06-02 10:47:47 +0530

    sum all values in array using seprate => func

--- session1.2.TheforEachContinued..js
+++ session1.2.TheforEachContinued..js
@@ -4,10 +4,12 @@ var numbers = [1, 2, 3, 4, 5];
 // Create a variable to hold the sum
 var sum = 0;
 
-// Loop over the array, incrementing the sum variable
-numbers.forEach((number) => {
+adder = (number) => {
   sum += number;
-});
+};
+
+// Loop over the array, incrementing the sum variable
+numbers.forEach(adder);
 
 // print the sum varibale
 console.log(sum);
