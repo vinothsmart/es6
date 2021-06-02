ebc6b9473ecb94eec18d5bf93f45c449bb8c26f3
vinothsmart <s.vinothkanna90@gmail.com>
2021-06-02 10:38:42 +0530

    sum all values in array

---
+++ session1.2.TheforEachContinued..js
@@ -0,0 +1,13 @@
+// Create an array of numbers
+var numbers = [1, 2, 3, 4, 5];
+
+// Create a variable to hold the sum
+var sum = 0;
+
+// Loop over the array, incrementing the sum variable
+numbers.forEach((number) => {
+  sum += number;
+});
+
+// print the sum varibale
+console.log(sum);
