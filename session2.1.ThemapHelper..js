870d4fb8881657060b94641aede59a2de5046b18
vinothsmart <s.vinothkanna90@gmail.com>
2021-11-25 07:47:19 +0530

    map function

--- session2.1.ThemapHelper..js
+++ session2.1.ThemapHelper..js
@@ -5,4 +5,9 @@ for (var i = 0; i < numbers.length; i++) {
   doubleNumbers.push(numbers[i] * 2);
 }
 
+var doubled = numbers.map((number) => {
+  return number * 2;
+});
+
 console.log(doubleNumbers);
+console.log(doubled);
