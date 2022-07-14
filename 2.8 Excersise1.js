6e94b6724551e862e353ab569d20839886c96a71
vinothsmart <s.vinothkanna90@gmail.com>
2022-07-14 13:21:58 +0530

    exe1

---
+++ 2.8 Excersise1.js
@@ -0,0 +1,15 @@
+function handlePosts() {
+  const posts = [
+    { id: 23, title: "Daily JS News" },
+    { id: 52, title: "Code Refactor City" },
+    { id: 105, title: "The Brightest Ruby" },
+  ];
+
+  //   for (var i = 0; i < posts.length; i++) {
+  //     savePost(posts[i]);
+  //   }
+
+  posts.forEach(function (post) {
+    savePost(post);
+  });
+}
