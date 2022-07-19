36de967a5393b286c87c60603b503c84d989616c
Vino@98 <42858372+Maribhuvan@users.noreply.github.com>
2022-07-19 13:23:02 +0530

    Update testing.js

--- testing.js
+++ testing.js
@@ -282,3 +282,122 @@ const expectedJson1 = [
     TestCases: "Case2_1,",
   },
 ];
+
+
+
+
+
+
+const jsonData = [
+  {
+    name: ["Root Suite"],
+    timestamp: ["2022-07-12T13:10:55"],
+    tests: ["0"],
+    file: [
+      "cypress/integration/carrierTest/details/audit/hideandUnhideAuditColumns.js",
+    ],
+    time: ["0.0000"],
+    failures: ["0"],
+  },
+  {
+    name: [
+      "Hide and Unhide Audit Columns[TA-8090,TA-8091,TA-8092,TA-8093,TA-8094,TA-8095]",
+    ],
+    timestamp: ["2022-07-12T13:10:55"],
+    tests: ["6"],
+    time: ["306.4580"],
+    failures: ["0"],
+    testcase: [
+      {
+        name: [
+          "Hide and Unhide Audit Columns[TA-8090,TA-8091,TA-8092,TA-8093,TA-8094,TA-8095] TA-8090 Hide and Unhide Type from Audit",
+        ],
+        time: ["49.4480"],
+        classname: ["TA-8090 Hide and Unhide Type from Audit"],
+      },
+      {
+        name: [
+          "Hide and Unhide Audit Columns[TA-8090,TA-8091,TA-8092,TA-8093,TA-8094,TA-8095] TA-8091 Hide and Unhide Change from Audit",
+        ],
+        time: ["51.1700"],
+        classname: ["TA-8091 Hide and Unhide Change from Audit"],
+      },
+      {
+        name: [
+          "Hide and Unhide Audit Columns[TA-8090,TA-8091,TA-8092,TA-8093,TA-8094,TA-8095] TA-8092 Hide and Unhide New Value from Audit",
+        ],
+        time: ["51.1310"],
+        classname: ["TA-8092 Hide and Unhide New Value from Audit"],
+      },
+      {
+        name: [
+          "Hide and Unhide Audit Columns[TA-8090,TA-8091,TA-8092,TA-8093,TA-8094,TA-8095] TA-8093 Hide and Unhide Date/Time from Audit",
+        ],
+        time: ["48.3730"],
+        classname: ["TA-8093 Hide and Unhide Date/Time from Audit"],
+      },
+      {
+        name: [
+          "Hide and Unhide Audit Columns[TA-8090,TA-8091,TA-8092,TA-8093,TA-8094,TA-8095] TA-8094 Hide and Unhide By from Audit",
+        ],
+        time: ["49.8400"],
+        classname: ["TA-8094 Hide and Unhide By from Audit"],
+      },
+      {
+        name: [
+          "Hide and Unhide Audit Columns[TA-8090,TA-8091,TA-8092,TA-8093,TA-8094,TA-8095] TA-8095 Hide and Unhide Description from Audit",
+        ],
+        time: ["49.6950"],
+        classname: ["TA-8095 Hide and Unhide Description from Audit"],
+      },
+    ],
+  },
+  {
+    name: ["Root Suite"],
+    timestamp: ["2022-07-12T13:09:54"],
+    tests: ["0"],
+    file: [
+      "cypress/integration/carrierTest/details/audit/expandAuditGridInCarrier.js",
+    ],
+    time: ["0.0000"],
+    failures: ["0"],
+  },
+  {
+    name: ["TA-8096"],
+    timestamp: ["2022-07-12T13:09:54"],
+    tests: ["1"],
+    time: ["52.5450"],
+    failures: ["0"],
+    testcase: [
+      {
+        name: ["TA-8096 TA8096  expand Audit Grid In Carrier"],
+        time: ["46.1400"],
+        classname: ["TA8096  expand Audit Grid In Carrier"],
+      },
+    ],
+  },
+];
+const FilterByFileAndTestCase = (data) => {
+    
+  return data.map((item) => {
+       const commonData = [];
+       const file=[];
+     if (item.tests.includes("0")) {
+             file.push(item.file[0])
+             file.push("")
+            commonData.push(file);
+       } else{
+          item.testcase.map((testCase) => {
+              let test=[];
+              test.push("");
+              test.push(testCase.classname[0]);
+              commonData.push(test);
+          });
+       }
+      
+    return commonData;
+  });
+};
+
+const output = [['filename','testcase'],...FilterByFileAndTestCase(jsonData).flat()];
+console.log(output);
