f9e5328127befd2ba4f57f27cf5f4391c7e5fd9d
vinothsmart <s.vinothkanna90@gmail.com>
2022-07-19 12:59:37 +0530

    updated

--- 3.9 The Map Helper.js
+++ 3.9 The Map Helper.js
@@ -1,2 +1,13 @@
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let doublenumbers = [];
+
+for (let i = 0; i < numbers.length; i++) {
+  doublenumbers.push(numbers[i] * 2);
+}
+
+const doubled = numbers.map(function(number) {  
+    return number * 2;
+});
+
+console.log(doublenumbers);
+console.log(doubled);
