c4590f5ae0751f99d953844d127bba5184de0fd3
vinothsmart <s.vinothkanna90@gmail.com>
2022-08-01 17:31:11 +0530

    map helper

--- 3.9 The Map Helper.js
+++ 3.9 The Map Helper.js
@@ -5,16 +5,9 @@ for (let i = 0; i < numbers.length; i++) {
   doublenumbers.push(numbers[i] * 2);
 }
 
-const doubled = numbers.map(function (number) {
+const doubled = numbers.map((number) => {
   return number * 2;
 });
 
 console.log(doublenumbers);
 console.log(doubled);
-
-const [menuData] = useState(["Menu1", "Menu2", "Menu3", "Menu4"]);
-<ul>
-  {menuData.map((item) => (
-    <li>{item}</li>
-  ))}
-</ul>;
