e0ef653edaad5836cf67628c52e4ba57c3df8743
vinothsmart <s.vinothkanna90@gmail.com>
2022-08-01 17:50:02 +0530

    3.11 execised

---
+++ 3.11 Excecise1.js
@@ -0,0 +1,16 @@
+const images = [
+  {
+    height: "34px",
+    width: "39px",
+  },
+  {
+    height: "39px",
+    width: "34px",
+  },
+  {
+    height: "83px",
+    width: "75px",
+  },
+];
+
+const filterHeught = images.map((image) => image.height);
