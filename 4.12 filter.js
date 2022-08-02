b18636ec19ce24a2c70e4ed895edcbd86ec46747
vinothsmart <s.vinothkanna90@gmail.com>
2022-08-02 10:50:02 +0530

    rm unwanted notes

--- 4.12 filter.js
+++ 4.12 filter.js
@@ -14,9 +14,3 @@ for (let i = 0; i < products.length; i++) {
 }
 
 console.log(filteredProdcuts);
-
-How I Structure My React Projects hooks
-
-https://www.google.com/search?q=How+I+Structure+My+React+Projects+hooks+&client=firefox-b-d&sxsrf=ALiCzsaPpUmNqYNovy5ZUE7n_z_mMHKZxw%3A1659366122025&ei=6urnYpmPAfKYjuMP1KGAkAs&ved=0ahUKEwiZrIXC9KX5AhVyjGMGHdQQALIQ4dUDCA0&uact=5&oq=How+I+Structure+My+React+Projects+hooks+&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIHCCEQoAEQCjIHCCEQoAEQCjIHCCEQoAEQCjoHCAAQRxCwAzoGCAAQHhAWOgUIABCGAzoFCCEQoAFKBAhBGABKBAhGGABQ9wZY_RZgnyFoAXABeACAAawCiAH6CpIBBzAuMy4zLjGYAQCgAQHIAQjAAQE&sclient=gws-wiz-serp
-
-https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwj557jw9KX5AhUyz6ACHaIGAP8QwqsBegQIEhAB&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DUUga4-z7b6s&usg=AOvVaw1XU4Ytaf8OYiuUf7znbq40
