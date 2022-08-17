149c25d139dd33ee690ef2018a5f856772d9f571
vinothsmart <s.vinothkanna90@gmail.com>
2022-08-17 19:24:31 +0530

    find using for

---
+++ 5.15 find.js
@@ -0,0 +1,20 @@
+const users = [
+  {
+    name: "John",
+  },
+  {
+    name: "Mary",
+  },
+  {
+    name: "Mike",
+  },
+];
+
+var user;
+for (var i = 0; i < users.length; i++) {
+  if (users[i].name === "John") {
+    user = users[i];
+  }
+}
+
+console.log(user);
