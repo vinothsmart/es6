eb00793d46c06ef6080640ebca6d346b332cedab
vinothsmart <s.vinothkanna90@gmail.com>
2022-10-13 15:29:15 +0530

    find ladder height

---
+++ 5.19 Excerise.js
@@ -0,0 +1,12 @@
+const ladders = [
+  { id: 1, height: "10 feet" },
+  { id: 2, height: "20 feet" },
+  { id: 3, height: "30 feet" },
+];
+
+const findWhere = (array, criteria) => {
+  const property = Object.keys(criteria)[0];
+  return array.find((element) => element[property] === criteria[property]);
+};
+
+findWhere(ladders, { height: "20 feet" });
