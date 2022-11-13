a1d701cd669842e24b50e727c41597c05e59b08e
vinothsmart <s.vinothkanna90@gmail.com>
2022-11-13 13:40:37 +0530

    exec18

--- 8.27let_constexec18.js
+++ 8.27let_constexec18.js
@@ -1,12 +1,12 @@
-var statuses = [
+const statuses = [
   { code: "OK", response: "Request successful" },
   { code: "FAILED", response: "There was an error with your request" },
   { code: "PENDING", response: "Your reqeust is still pending" },
 ];
-var message = "";
-var currentCode = "OK";
+let message = "";
+const currentCode = "OK";
 
-for (var i = 0; i < statuses.length; i++) {
+for (let i = 0; i < statuses.length; i++) {
   if (statuses[i].code === currentCode) {
     message = statuses[i].response;
   }
