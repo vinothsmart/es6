6fa32bfc40aac35cd03b265e7575a01c323b58f3
vinothsmart <s.vinothkanna90@gmail.com>
2022-11-13 13:16:29 +0530

    let and const

---
+++ 8.26let_const1.js
@@ -0,0 +1,12 @@
+// var name = "John";
+// var title = "React Developer";
+// var hourlyWage = 40;
+
+// ES6
+const name = "John";
+let title = "React Developer";
+let hourlyWage = 40;
+
+// Some times
+title = "Senior React Developer";
+hourlyWage = 45;
