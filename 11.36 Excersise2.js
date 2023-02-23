1e66e3b429d1c4cf65209934a0a4cae439d0f286
vinothsmart <s.vinothkanna90@gmail.com>
2023-02-23 10:25:50 +0530

    exec2

---
+++ 11.36 Excersise2.js
@@ -0,0 +1,7 @@
+const fields = ["firstName", "lastName", "phoneNumber"];
+
+const props = { fields: fields };
+
+console.log(props.fields[0]);
+console.log(props.fields[1]);
+console.log(props.fields[2]);
