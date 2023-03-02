3cea11eae44310cf45768061e6abbcd44958efe2
vinothsmart <s.vinothkanna90@gmail.com>
2023-03-02 11:46:39 +0530

    daault fucntion

---
+++ 11.39defaultfunction.js
@@ -0,0 +1,7 @@
+function makeAjaxRequest(url, method = "GET") {
+  return method;
+  // logic to make the request
+}
+
+console.log(makeAjaxRequest("google.com"));
+console.log(makeAjaxRequest("google.com", "POST"));
