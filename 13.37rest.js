ba6f099accd12d0543eee0426c26914edde9b0a3
vinothsmart <s.vinothkanna90@gmail.com>
2023-03-13 11:34:10 +0530

    this is another method

--- 13.37rest.js
+++ 13.37rest.js
@@ -1,3 +1,6 @@
-const addNumbers = (num) => num.reduce((a, b) => a + b, 0);
+const addNumbers = (a, b, c, d, e) => {
+  const numbers = [a, b, c, d, e];
+  return numbers.reduce((sum, number) => sum + number, 0);
+};
 
-console.log(addNumbers([1, 2, 3, 4, 5]));
+console.log(addNumbers(1, 2, 3, 4, 5));
