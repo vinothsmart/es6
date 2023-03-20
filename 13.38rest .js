b9ad70e790a3bbcdadab29b620ca4977d6c5eb86
vinothsmart <s.vinothkanna90@gmail.com>
2023-03-20 11:36:36 +0530

    concat using spread

---
+++ 13.38rest .js
@@ -0,0 +1,7 @@
+const defaultColors = ["red", "green"];
+const userFavoriteColors = ["orange", "yellow"];
+const fallColors = ["fire red", "fall orange"];
+
+console.log(defaultColors.concat(userFavoriteColors));
+
+console.log([...fallColors, ...defaultColors, ...userFavoriteColors]);
