cd59361ce70b5e76d9842d0eba61dfe3fab1d6ee
vinothsmart <s.vinothkanna90@gmail.com>
2023-05-09 18:03:35 +0530

    destruting

---
+++ 14.40 Destructing.js
@@ -0,0 +1,13 @@
+var expense = {
+  type: "Business",
+  amount: "$45 USD",
+};
+
+// var type = expense.type;
+// var amount = expense.amount;
+
+// ES6
+const { type, amount } = expense;
+
+console.log(type);
+console.log(amount);
