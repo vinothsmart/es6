5e8038bf3628c002dfb1413df6e5ea2e765ab41f
vinothsmart <s.vinothkanna90@gmail.com>
2023-05-09 16:04:34 +0530

    rest and spread ans

--- 13.40 Execerise3.js
+++ 13.40 Execerise3.js
@@ -1,3 +1,9 @@
-function unshift(array, a, b, c, d, e) {
-  return [a, b, c, d, e].concat(array);
-}
+// function unshift(array, a, b, c, d, e) {
+//   return [a, b, c, d, e].concat(array);
+// }
+
+const unshift = (array, ...rest) => {
+  return [...rest, ...array];
+};
+
+console.log(unshift([1, 2, 3], 4, 5, 6, 7, 8));
