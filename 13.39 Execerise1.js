c337f0f449a9afb893ff7d72246c037556e28f39
vinothsmart <s.vinothkanna90@gmail.com>
2023-05-09 15:54:56 +0530

    using rest opertor

---
+++ 13.39 Execerise1.js
@@ -0,0 +1,15 @@
+// Refactor the following function to use the rest operator.  Remember, an argument using the rest operator does *not* need to be called 'rest'.
+
+// function product(a, b, c, d, e) {
+//   var numbers = [a, b, c, d, e];
+
+//   return numbers.reduce(function (acc, number) {
+//     return acc * number;
+//   }, 1);
+// }
+
+const product = (...numbers) => {
+  return numbers.reduce((acc, number) => acc * number, 1);
+};
+
+console.log(product(1, 2, 3, 4, 5));
