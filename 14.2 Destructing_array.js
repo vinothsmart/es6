21f8a1d69ae042ff4bca7e0cfc75ce64aacc04e2
vinothsmart <s.vinothkanna90@gmail.com>
2023-05-22 12:22:15 +0530

    array desctruc

---
+++ 14.2 Destructing_array.js
@@ -0,0 +1,11 @@
+const compaines = ["Google", "Facebook", "Uber"];
+
+const [name1, name2, name3, name4] = compaines;
+const firstCompany = compaines[0];
+
+console.log(name1);
+console.log(name2);
+console.log(name3);
+console.log(name4);
+
+console.log(firstCompany);
