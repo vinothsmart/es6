d59732cb2b19cb54d7baaea0f15e647159124a53
vinothsmart <s.vinothkanna90@gmail.com>
2023-05-30 18:48:15 +0530

    destruct first array

--- 14.43 Destructing_array_object.js
+++ 14.43 Destructing_array_object.js
@@ -1,13 +1,23 @@
-const compaines = [
-  { name: "Google", location: "Mountain View" },
-  { name: "Facebook", location: "Menlo Park" },
-  { name: "Uber", location: "San Francisco" },
-];
+// const compaines = [
+//   { name: "Google", location: "Mountain View" },
+//   { name: "Facebook", location: "Menlo Park" },
+//   { name: "Uber", location: "San Francisco" },
+// ];
 
-// using es5
-// const location = compaines[0].location;
-// console.log(location);
+// // using es5
+// // const location = compaines[0].location;
+// // console.log(location);
 
-const [{ location }] = compaines;
+// const [{ location }] = compaines;
 
-console.log(location, location2);
+// console.log(location, location2);
+
+const Google = {
+  locations: ["Mountain View", "New York", "London"],
+};
+
+const {
+  locations: [location],
+} = Google;
+
+console.log(location);
