6323ae7944b16f970a1a297be87efd6edc298685
vinothsmart <s.vinothkanna90@gmail.com>
2023-05-30 18:36:13 +0530

    array object

---
+++ 14.43 Destructing_array_object.js
@@ -0,0 +1,8 @@
+const compaines = [
+  { name: "Google", location: "Mountain View" },
+  { name: "Facebook", location: "Menlo Park" },
+  { name: "Uber", location: "San Francisco" },
+];
+
+// using es5
+const location = compaines[0].location;
