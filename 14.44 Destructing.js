580c02ec3889ba83e2b0fd8fc786dcf67de6749f
vinothsmart <s.vinothkanna90@gmail.com>
2023-06-12 15:54:05 +0530

    destrucring

--- 14.44 Destructing.js
+++ 14.44 Destructing.js
@@ -1,7 +1,18 @@
-function signUp(username, password, email, dateOfBirth, city) {
+function signUp({ username, password, email, dateOfBirth, city }) {
   // create new user
 }
 
 // other code
 
-signUp("myname", "mypassword", "demo@gmail.com", "1/1/1990", "New York");
+// signUp("myname", "mypassword", "demo@gmail.com", "1/1/1990", "New York");
+
+// after destructing
+const user = {
+  username: "myname",
+  password: "mypassword",
+  email: "demo@gmail.com",
+  dateOfBirth: "1/1/1990",
+  city: "New York",
+};
+
+signUp(user);
