e5bd0f2843de4b72d10d8c26f114ec266dfaf6c7
vinothsmart <s.vinothkanna90@gmail.com>
2023-06-12 15:52:09 +0530

    func declarre

---
+++ 14.44 Destructing.js
@@ -0,0 +1,7 @@
+function signUp(username, password, email, dateOfBirth, city) {
+  // create new user
+}
+
+// other code
+
+signUp("myname", "mypassword", "demo@gmail.com", "1/1/1990", "New York");
