db34244d4c74b342a8f8a1ec45741516a6de3c62
vinothsmart <s.vinothkanna90@gmail.com>
2023-06-12 17:05:51 +0530

    class pending

---
+++ 15.47 Classes.js
@@ -0,0 +1,11 @@
+function Car(options) {
+  this.title = options.title;
+}
+
+Car.prototype.drive = function () {
+  return "vroom";
+};
+
+const car = new Car({ title: "Focus" });
+console.log(car.drive());
+console.log(car);
