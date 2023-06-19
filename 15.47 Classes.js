ba3cab830dc641902cd0978f219d73433a1af32d
vinothsmart <s.vinothkanna90@gmail.com>
2023-06-19 16:01:23 +0530

    pending

--- 15.47 Classes.js
+++ 15.47 Classes.js
@@ -6,6 +6,14 @@ Car.prototype.drive = function () {
   return "vroom";
 };
 
-const car = new Car({ title: "Focus" });
-console.log(car.drive());
-console.log(car);
+// const car = new Car({ title: "Focus" });
+// console.log(car.drive());
+// console.log(car);
+
+function Toyota(options) {
+  this.color = options.color;
+}
+
+const toyota = new Toyota({ color: "red", title: "Daily Driver" });
+
+console.log(toyota);
