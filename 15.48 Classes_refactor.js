a4a8537c759a39393399d1f329ddfe172c9a4f15
vinothsmart <s.vinothkanna90@gmail.com>
2023-06-25 11:20:32 +0530

    classes

---
+++ 15.48 Classes_refactor.js
@@ -0,0 +1,12 @@
+class Car {
+  constructor({ title }) {
+    this.title = title;
+  }
+  drive() {
+    return "vroom";
+  }
+}
+
+const car = new Car({ title: "Toyota" });
+console.log(car);
+console.log(car.drive());
