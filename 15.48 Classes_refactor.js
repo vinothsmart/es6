b49f1f66d03fccb211237d9cf225bdc04e4aade7
vinothsmart <s.vinothkanna90@gmail.com>
2023-07-03 13:39:33 +0530

    completed

--- 15.48 Classes_refactor.js
+++ 15.48 Classes_refactor.js
@@ -7,6 +7,24 @@ class Car {
   }
 }
 
-const car = new Car({ title: "Toyota" });
-console.log(car);
-console.log(car.drive());
+// const car = new Car({ title: "Toyota" });
+// console.log(car);
+// console.log(car.drive());
+
+class Toyota extends Car {
+  constructor(options) {
+    super(options); // Car.constructor()
+    this.color = options.color;
+  }
+  honk() {
+    return "beep";
+  }
+}
+
+const toyota = new Toyota({
+  color: "red",
+  title: "Daily Driver",
+});
+console.log("-----");
+console.log(toyota.honk());
+console.log(toyota);
