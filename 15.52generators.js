6bf57c82fc1568693201a403976b792d2aeb96c9
vinothsmart <s.vinothkanna90@gmail.com>
2023-10-17 10:25:25 +0530

    another gen

--- 15.52generators.js
+++ 15.52generators.js
@@ -6,12 +6,20 @@ function* shopping() {
   // go into the store with cash
   const stuffFromStore = yield "cash";
 
+  // walking to laundry place
+  const cleanClothes = yield "laundry";
+
   // walking back home
-  return stuffFromStore;
+  return [stuffFromStore, cleanClothes];
 }
 
 // stuff in the store
 const gen = shopping();
 
 console.log(gen.next()); // leaving our house
+// walked into the store
+// walking up and down the aisles..
+// purchase our stuff
+
 console.log(gen.next("groceries")); // leaving the store with groceries
+console.log(gen.next("clean clothes"));
