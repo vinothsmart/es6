ba310be1f4fd7f751905d1f7fdb2a7c77a702cb8
vinothsmart <s.vinothkanna90@gmail.com>
2024-01-04 19:34:18 +0530

    promise completed

--- 16.56Promises.js
+++ 16.56Promises.js
@@ -1,11 +1,20 @@
-const promise = new Promise((resolve, reject) => {
-  reject();
-});
 // three states of promises
 // 1. unresolved or pending
 // 2. resolved or fulfilled
 // 3. rejected or failed
-console.log(promise);
+// console.log(promise);
+
+const promise = new Promise((resolve, reject) => {
+  //   reject();
+  //   setTimeout(() => {
+  //     resolve();
+  //   }, 3000);
+  var request = new XMLHttpRequest();
+  // make a request
+  request.onload = () => {
+    resolve();
+  };
+});
 
 promise
   .then(() => console.log("finally finished!"))
